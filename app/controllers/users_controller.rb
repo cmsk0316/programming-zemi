b02b793cfc1eea54420d5449ebe39135122631cd
cmsk0316 <masakichinen0316@i.softbank.jp>
2021-10-17 16:17:25 +0000

    validation and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params)   # 実装は終わっていないことに注意!
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
@@ -14,7 +18,7 @@ class UsersController < ApplicationController
 
   private
 	
-	def user_params
-		params.require(:user).permit(:name, :email)
-	end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
